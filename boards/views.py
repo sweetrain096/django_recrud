2126ce7d49c02d8a941a8e86458eb6386a3edee3
edutak <edutak.ssafy@gmail.com>
2019-03-11 05:32:11 +0000

    00_CRUD

---
+++ boards/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render, redirect
+from .models import Board
+# Create your views here.
+def index(request):
+    boards = Board.objects.order_by('-id')
+    return render(request, 'boards/index.html', {
+        'boards': boards
+    })
+    
+def new(request):
+    return render(request, 'boards/new.html')
+    
+def create(request):
+    board = Board()
+    board.title = request.POST.get('title')
+    board.content = request.POST.get('content')
+    board.save()
+    return redirect(f'/boards/{board.pk}')
+
+def detail(request, pk):
+    board = Board.objects.get(pk=pk)
+    return render(request, 'boards/detail.html', {
+        'board': board
+    })
+    
+def delete(request, pk):
+    board = Board.objects.get(pk=pk)
+    board.delete()
+    return redirect('/boards')
+
+def edit(request, pk):
+    board = Board.objects.get(pk=pk)
+    return render(request, 'boards/edit.html', {
+        'board': board
+    })
+
+def update(request, pk):
+    board = Board.objects.get(pk=pk)
+    board.title = request.POST.get('title')
+    board.content = request.POST.get('content')
+    board.save()
+    return redirect(f'/boards/{board.pk}')
+
+
+
+
+
+
+
+
